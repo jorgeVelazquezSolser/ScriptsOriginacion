854bad8b763e68666b0f7ff8d544706985a26378
hguerrerog12 <hguerrerog12@hotmail.com>
2026-05-09 11:24:37 -0600

    Se suben scripts para DB WebSec. Listos para producción

---
+++ WebSec/tabla/SegPerfilesObjetosEtapas.sql
@@ -0,0 +1,14 @@
+USE WebSec
+
+insert into SegPerfilesObjetosEtapas values (2 , '03', '07', 3037, '01', '01', '001' )
+insert into SegPerfilesObjetosEtapas values (3 , '03', '07', 3037, '01', '01', '001' )
+insert into SegPerfilesObjetosEtapas values (4 , '03', '07', 3037, '01', '01', '001' )
+insert into SegPerfilesObjetosEtapas values (30, '03', '07', 3037, '01', '01', '001' )
+insert into SegPerfilesObjetosEtapas values (2 , '04', '08', 3037, '01', '01', '001' )
+insert into SegPerfilesObjetosEtapas values (3 , '04', '08', 3037, '01', '01', '001' )
+insert into SegPerfilesObjetosEtapas values (4 , '04', '08', 3037, '01', '01', '001' )
+insert into SegPerfilesObjetosEtapas values (30, '04', '08', 3037, '01', '01', '001' )
+insert into SegPerfilesObjetosEtapas values (2 , '06', '34', 3037, '01', '01', '001' )
+insert into SegPerfilesObjetosEtapas values (3 , '06', '34', 3037, '01', '01', '001' )
+insert into SegPerfilesObjetosEtapas values (4 , '06', '34', 3037, '01', '01', '001' )
+insert into SegPerfilesObjetosEtapas values (30, '06', '34', 3037, '01', '01', '001' )
